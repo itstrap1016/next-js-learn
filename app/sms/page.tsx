067282114eba5d8441ee1a4c9a8467e0df2f3951
이한승 <itstrap1016@gmail.com>
2025-08-08 23:12:36 +0900

    first commit

---
+++ app/sms/page.tsx
@@ -0,0 +1,30 @@
+import Button from "@/components/button";
+import Input from "@/components/input";
+
+export default function SMSLogin() {
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">SMS Log in</h1>
+        <h2 className="text-xl">Verify your phone number.</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <Input
+          name="phone-number"
+          type="number"
+          placeholder="Phone number"
+          required
+          errors={[]}
+        />
+        <Input
+          name="verification-code"
+          type="number"
+          placeholder="Verification code"
+          required
+          errors={[]}
+        />
+        <Button text="Verify" />
+      </form>
+    </div>
+  );
+}
